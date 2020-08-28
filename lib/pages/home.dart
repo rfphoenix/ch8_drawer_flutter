dce863a9845f89a8cfe3c7d3808e946ffcb19bb7
Johnson Wang <freelance@rfphoenix.com>
2020-08-27 22:12:41 -0700

    - remove unnecessary package imports

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ch8_drawer_flutter/widgets/birthdays.dart';
-import 'package:ch8_drawer_flutter/widgets/gratitude.dart';
-import 'package:ch8_drawer_flutter/widgets/reminders.dart';
 import 'package:ch8_drawer_flutter/widgets/left_drawer.dart';
 import 'package:ch8_drawer_flutter/widgets/right_drawer.dart';
 
